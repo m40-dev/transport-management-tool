d4f166e1b292fa93a06d0127601b33e4669fc6a6
Tomek <tomasz.s@protonmail.com>
2023-11-08 21:31:01 +0100

    updated application version to 0.7.3

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from lib.ProgramConfiguration import ProgramConfiguration, ObjectConfiguration, 
 #""" Database Connector Module """
 from lib.db.database import DatabaseConnection
 
-VERSION = '0.7.2'
+VERSION = '0.7.3'
 
 class Transport_Manager(QMainWindow):
     """Main window class for connection launcher"""
